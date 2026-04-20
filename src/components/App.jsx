17071628f10adbe29c652c45b314b76ed22a83d9
VietDucNg <vietducnguyen.fit@gmail.com>
2026-04-20 15:51:32 +0200

    build: add header to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import '../styles/reset.css'
 import '../styles/style.css'
+import '../styles/App.css'
+import Header from './Header'
 
 function App() {
-
+  return (
+  <div className='app-div'>
+    <Header />
+  </div>
+  )
 }
 
 export default App
