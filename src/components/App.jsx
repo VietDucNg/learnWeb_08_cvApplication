3fe91acfa69c2134c170acba3d9da80f8777058f
VietDucNg <vietducnguyen.fit@gmail.com>
2026-04-21 13:51:19 +0200

    style: input-div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,17 @@ import '../styles/reset.css'
 import '../styles/style.css'
 import '../styles/App.css'
 import Header from './Header'
+import GenInfoDiv from './GenInfoDiv'
+
+
 
 function App() {
   return (
   <div className='app-div'>
     <Header />
+    <main>
+      <GenInfoDiv />
+    </main>
   </div>
   )
 }
