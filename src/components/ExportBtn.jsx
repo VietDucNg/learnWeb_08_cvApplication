9c641c8db26efa73592953aba63635a2e0cafa80
VietDucNg <vietducnguyen.fit@gmail.com>
2026-05-02 18:58:12 +0200

    build: uninstall lucide

--- src/components/ExportBtn.jsx
+++ src/components/ExportBtn.jsx
@@ -1,10 +1,15 @@
 import { MdOutlineFileDownload } from "react-icons/md";
+import { Download } from "lucide-react";
 import "../styles/ExportBtn.css";
+import Button from "@mui/material/Button";
 
 export default function ExportBtn() {
   return (
-    <button className="export-btn flex-row">
+    <Button className="export-btn" variant="contained">
       <MdOutlineFileDownload /> Export PDF
-    </button>
+    </Button>
+    // <button className="export-btn flex-row">
+    //   <MdOutlineFileDownload /> Export PDF
+    // </button>
   );
 }
