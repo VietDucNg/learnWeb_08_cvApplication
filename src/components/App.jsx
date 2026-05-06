2d56eb2befcd53d7299dff2b74168960eeefee63
VietDucNg <vietducnguyen.fit@gmail.com>
2026-05-06 20:28:54 +0200

    feat!: add localStorage for genInfoData

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,35 @@
 import "./reset.css";
 import "./style.css";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header/Header";
 import GenInfoDiv from "./GenInfoDiv/GenInfoDiv";
 import LiveCV from "./liveCV/LiveCV";
 
 function App() {
-  const initialInfoData = {
-    name: "Viet Nguyen",
-    phone: "+49 123 456 7890",
-    email: "viet.nguyen@webdev.com",
-    location: "Greifswald, Germany",
-    title: "WebGIS developer",
+  // handle general info data
+  const initialInfoData = function () {
+    const data = JSON.parse(localStorage.getItem("infoData"));
+    if (data) return data;
+    else {
+      return {
+        name: "Viet Nguyen",
+        phone: "+49 123 456 7890",
+        email: "viet.nguyen@webdev.com",
+        location: "Greifswald, Germany",
+        title: "WebGIS developer",
+      };
+    }
   };
+
   const [infoData, setInfoData] = useState(initialInfoData);
+
+  useEffect(
+    function () {
+      localStorage.setItem("infoData", JSON.stringify(infoData));
+    },
+    [infoData],
+  );
 
   return (
     <div className="app-div">
--- src/components/GenInfoDiv/GenInfoForm.jsx
+++ src/components/GenInfoDiv/GenInfoForm.jsx
@@ -47,7 +47,6 @@ export default function GenInfoForm({ setResetForm, setInfoData }) {
   }
 
   // send clear all function to App
-
   useEffect(
     function () {
       setResetForm(() => reset);
