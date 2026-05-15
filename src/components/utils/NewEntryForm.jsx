f70131c960bc1b2f68c12502ee7dd6cd6d662f0d
VietDucNg <vietducnguyen.fit@gmail.com>
2026-05-15 15:34:11 +0200

    feat!: add mui date picker to NewEntryForm

--- src/components/utils/NewEntryForm.jsx
+++ src/components/utils/NewEntryForm.jsx
@@ -2,6 +2,12 @@ import { useForm } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import "./NewEntryForm.css";
 
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+import dayjs from "dayjs";
+
 const TextFieldSx = {
   "& .MuiInputLabel-root": {
     color: "var(--font-color)",
@@ -18,12 +24,33 @@ const TextFieldSx = {
   },
 };
 
+const slotPropsDatePicker = {
+  textField: {
+    sx: {
+      color: "var(--line-break-color)",
+      borderRadius: "5px",
+      borderWidth: "1px",
+      borderColor: "var(--line-break-color)",
+      border: "1px solid",
+      backgroundColor: "var(--section-background-color)",
+      "& .MuiInputLabel-root, & svg, & span": {
+        color: "var(--font-color)",
+      },
+    },
+  },
+};
+
+const currentYear = dayjs();
+
 function onSubmit(data) {
   console.log(data);
 }
 
 export default function NewEntryForm() {
   const { register, handleSubmit } = useForm();
+
+  const [fromDate, setFromDate] = useState(dayjs());
+  const [toDate, setToDate] = useState(dayjs());
 
   return (
     <section className="newEntryDiv">
@@ -46,6 +73,32 @@ export default function NewEntryForm() {
           variant="outlined"
           {...register("title")}
         />
+        <div className="datePickerGroup flex-row">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              slotProps={slotPropsDatePicker}
+              label="From"
+              name="fromDate"
+              maxDate={currentYear}
+              openTo="year"
+              views={["year", "month"]}
+              value={fromDate}
+              onChange={(newDate) => setFromDate(newDate)}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              slotProps={slotPropsDatePicker}
+              label="To"
+              name="toDate"
+              maxDate={currentYear}
+              openTo="year"
+              views={["year", "month"]}
+              value={toDate}
+              onChange={(newDate) => setToDate(newDate)}
+            />
+          </LocalizationProvider>
+        </div>
       </form>
     </section>
   );
